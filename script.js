08e5c0078c92bd1de1505f6d3e5c3c75abcb24f6
町栄地区座談会 <choei.zadankai@gmail.com>
2025-05-13 19:14:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let currentIndex = 0;
 let autoInterval;
 let currentSpeed = 0.9;
 
-// ✅ 読み替えマップ：読み間違いそうな単語だけ追加してね
+// ✅ 読み替えマップ：読み間違いそうな単語だけ追加
 const readingReplacements = {
   "町栄地区": "ちょうえいちく",
   "学会歌":"がっかいか",
